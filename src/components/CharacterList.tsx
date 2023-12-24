b0c6f4d1ef6dd7bc226e005be11ac016c5c07138
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2023-12-24 17:51:56 -0500

    fix(rick-and-morty): loading

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -5,10 +5,8 @@ import { Character } from '@/interfaces/rick-and-morty-api-response';
 
 export const CharacterList = () => {
   const { data: { results: characters }, loading, error }: Response = useFetch();
-  if (loading) return <p>CARGANDO DUDE</p>;
-
+  if (loading) return <p>Cargando...</p>;
   if (error) return <p>Ha ocurrido un error</p>;
-
 
   return (
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
